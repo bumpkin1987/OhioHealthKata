2923aaf9e0a01e8adc4d25a5552fb284689ff338
Mat Boyles <bumpkin1987@gmail.com>
2020-10-21 01:51:30 -0400

    refactored generateNightlyCharge to make readability easier to follow.

--- src/main/java/babySitterChargingApp/shiftGenerator.java
+++ src/main/java/babySitterChargingApp/shiftGenerator.java
@@ -1,9 +1,5 @@
 package BabySitterChargingApp;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class ShiftGenerator {
 
     String startingTime, sleepyTime, endingTime;
@@ -22,7 +18,7 @@ public class ShiftGenerator {
         getEndTime();
         if (timeInputsInChronologicalOrder(startingHour, bedtimeHour, endingHour)) {
             generateNightlyCharge(startingHour, bedtimeHour, endingHour);
-            calculateChargeAndDisplay(nightlyCharge);
+            display(nightlyCharge);
         }
         else {
             System.out.println("You must put the times in order.");
@@ -31,9 +27,9 @@ public class ShiftGenerator {
 
     public int getStartTime() {
         StartTime startTime = new StartTime();
-        startingTime = startTime.getUserShiftInfo();
-        startingHour = startTime.generateHourInteger(startingTime);
-        startingHour = convertHoursPastMidnight(startingHour);
+        startingTime = startTime.getUserShiftInfo(); // getting and validating the users time input.
+        startingHour = startTime.generateHourInteger(startingTime); // convert to int for pay calculations.
+        startingHour = convertHoursPastMidnight(startingHour); // midnight to 4am will be represented as 24 - 28 for easy calculation.
         return startingHour;
     }
 
@@ -41,7 +37,7 @@ public class ShiftGenerator {
         BedTime bedTime = new BedTime();
         sleepyTime = bedTime.getUserShiftInfo();
         bedtimeHour = bedTime.generateHourInteger(sleepyTime);
-        bedtimeHour = convertHoursPastMidnight(bedtimeHour);
+        bedtimeHour = convertHoursPastMidnight(bedtimeHour); 
         return bedtimeHour;
     }
 
@@ -53,7 +49,7 @@ public class ShiftGenerator {
         return endingHour;
     }
 
-    private boolean timeInputsInChronologicalOrder(int shiftStart, int shiftBed, int shiftEnd) {
+    private boolean timeInputsInChronologicalOrder(int shiftStart, int shiftBed, int shiftEnd) { // making sure times uer puts in are in chronological order. -> start <= bedtime <= end.
         if (shiftStart == shiftEnd) {
             return true;
         }
@@ -71,20 +67,20 @@ public class ShiftGenerator {
 
         int beginning = Math.min(shiftStart, MIDNIGHT);
 
-        if (beginning == 24) {
+        if (beginning == MIDNIGHT) { // if shift starts at midnight what ever the difference betwen start and end is how many hours to bill at 16$/hr.
             return (shiftEnd - shiftStart) * AFTER_MIDNIGHT_PAYRATE;
         } 
-        else {
-            int afterMidnightRate = 0;
-            int numHoursBeforeBed = Math.min(shiftBed, MIDNIGHT); // grab which ever value is less
-            int numHoursInBedBeforeMidnight = Math.min(MIDNIGHT, shiftEnd); // 
-            int beforeBedRate = (numHoursBeforeBed - shiftStart) * NORMAL_PAYRATE;
-            int afterBedRate = (numHoursInBedBeforeMidnight - numHoursBeforeBed) * BEDTIME_PAYRATE;
+        else { // all scenarios that have hours to be billed at before midniht either 12$/hr or 16$/hr.
+            int after_Midnight_Charge_Amount = 0;                     
+            int num_Hours_Before_Bed = Math.min(shiftBed, MIDNIGHT); // grab which ever value is less (PRE MIDNIGHT) to calculate hours in bed and normal hours.
+            int num_Hours_In_Bed_Before_Midnight = Math.min(MIDNIGHT, shiftEnd); // grab which ever value 
+            int normal_Hours_Charge_Amount = (num_Hours_Before_Bed - shiftStart) * NORMAL_PAYRATE;
+            int bedtime_Hours_Charge_Amount = (num_Hours_In_Bed_Before_Midnight - num_Hours_Before_Bed) * BEDTIME_PAYRATE;
 
             if (shiftEnd >= 24) {
-                afterMidnightRate = (shiftEnd - MIDNIGHT) * AFTER_MIDNIGHT_PAYRATE;
+                after_Midnight_Charge_Amount = (shiftEnd - MIDNIGHT) * AFTER_MIDNIGHT_PAYRATE;
             }
-            nightlyCharge =  beforeBedRate + afterBedRate + afterMidnightRate;
+            nightlyCharge =  normal_Hours_Charge_Amount + bedtime_Hours_Charge_Amount + after_Midnight_Charge_Amount;
         }
 
         return nightlyCharge;
@@ -94,12 +90,12 @@ public class ShiftGenerator {
       // whatever morning hour is procesed midnight through 4:00 am we just add 24
       public int convertHoursPastMidnight(int hourNum) {
         if (hourNum >= 0 && hourNum <= 4) {
-            hourNum += 24;
+            hourNum += MIDNIGHT;
         }
         return hourNum;
     }
 
-    public void calculateChargeAndDisplay(int  nightlyCharge) {
+    public void display(int  nightlyCharge) {
 
         if (nightlyCharge == 0) {
             System.out.println("You may have entered times out of order, or you didn't work...");
